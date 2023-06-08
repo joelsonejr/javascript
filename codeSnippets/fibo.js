a540abe703c0d2d41c10790d53f116606f26d090
Joelson Junior <joelson.ejr@gmail.com>
2023-06-08 19:42:52 -0300

    Corrected the identation of the function fibo.

---
+++ codeSnippets/fibo.js
@@ -0,0 +1,23 @@
+//Function to execute the Fibonacci sequence for a define number of times. This number is indicated by the function input.
+
+let interations = 7;
+
+function fibo(interations) {
+	let fiboSequence=[];
+
+	for(i=0; i<interations; i++){
+
+		if(fiboSequence.length == 0){
+			fiboSequence[0] = 0;
+		} else if(fiboSequence.length == 1){  //TODO: add verf in case the first element isn't 1.
+			fiboSequence[1] = 1;
+		} else {
+				let joe = fiboSequence[fiboSequence.length-1] + fiboSequence[fiboSequence.length-2];
+				fiboSequence.push(joe)
+		}
+
+	}
+
+	return(fiboSequence);
+}
+
